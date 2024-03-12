404b8fa9f2c11d1777524be33fb3a0163b97dee5
Tom ten Thij <code@tomtenthij.nl>
2024-03-12 13:37:56 +0100

    Create a tech page to show some info relevant to dev

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "sortable", to: "pages#sortable"
+  get "tech", to: "pages#tech"
   # Defines the root path route ("/")
   root "pages#home"
 end
