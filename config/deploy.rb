10ca36c5fd47d0f0a03066b581731be0869897b8
Tom ten Thij <code@tomtenthij.nl>
2024-03-18 14:11:28 +0000

    Move repo locally

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
-set :master_key_local_path, "/Users/me/created/projects/patagonia/open_letter/config/master.key"
+set :master_key_local_path, "/Users/me/created/projects/make_fishing_fair/make_fishing_fair/config/master.key"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
