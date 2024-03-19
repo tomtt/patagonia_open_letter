c259def01d1e33bb0012433454dad7c3a0612f98
Tom ten Thij <code@tomtenthij.nl>
2024-03-19 07:50:41 +0000

    Another content block, rendered with message if missing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    @open_letter_content = ContentBlock.find_by_key!("open_letter").content
+    @page_introduction_content = content_for_key("page_introduction")
+    @open_letter_content = content_for_key("open_letter")
     @supporting_organisations = SupportingOrganisation.all
     @signatories = Signatory.all
+  end
+
+  private
+
+  def  content_for_key(key)
+    ContentBlock.find_by_key(key)&.content || "**Here will be the content for \"#{key}\"**"
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,6 @@
 module PagesHelper
+  def markdown_render(content)
+    @@markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    @@markdown.render(content).html_safe
+  end
 end
