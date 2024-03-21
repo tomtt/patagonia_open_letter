dc175815c9790783eef25921abedba14109e2439
Tom ten Thij <code@tomtenthij.nl>
2024-03-21 13:08:39 +0000

    Content_block seed more verbose

--- db/seeds/content_blocks_seed.rb
+++ db/seeds/content_blocks_seed.rb
@@ -3,6 +3,7 @@
 open_letter_content = File.read(Rails.root.join("db", "seeds", "open_letter_content.md"))
 page_introduction_content = File.read(Rails.root.join("db", "seeds", "page_introduction_content.md"))
 
-puts "Creating open letter content_block"
+puts "Creating open_letter content_block"
 ContentBlock.find_or_create_by(key: "open_letter", content: open_letter_content)
+puts "Creating page_introduction content_block"
 ContentBlock.find_or_create_by(key: "page_introduction", content: page_introduction_content)
