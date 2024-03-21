c6c5dc64f4cb4c75099711f700f744d69d825f64
Tom ten Thij <code@tomtenthij.nl>
2024-03-21 13:30:49 +0000

    Make dashboard page relevant

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,38 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Info" do
+          para "There are #{ SupportingOrganisation.count } supporting organisations"
+          ul do
+            SupportingOrganisation.all.order(:name).map do |supporting_organisation|
+              li do
+                link_to(supporting_organisation.name, admin_supporting_organisation_path(supporting_organisation))
+              end
+            end
+          end
+          para link_to("Check relative logo sizes", supporting_organisations_path)
+          para "There are #{ Signatory.count } signatories"
+        end
+      end
+
+      column do
+        panel "Content blocks defined" do
+          table do
+            ContentBlock.all.order(:key).map do |content_block|
+              tr do
+                td do
+                  link_to(content_block.key, admin_content_block_path(content_block))
+                end
+                td do
+                  content_block.content.truncate(80)
+                end
+              end
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
