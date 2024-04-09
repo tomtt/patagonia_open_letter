fc70ca91143f56b95ab0ed61146a0618bf8e1d13
Tom ten Thij <code@tomtenthij.nl>
2024-04-09 11:48:57 +0200

    Some layout tweaks suggested by G

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,8 +5,8 @@ class PagesController < ApplicationController
     @page_introduction_content = content_for_key("page_introduction")
     @open_letter_content = content_for_key("open_letter")
     @the_ask_content = content_for_key("the_ask")
-    @supporting_organisations = SupportingOrganisation.all.has_logo
-    @signatories = Signatory.all
+    @supporting_organisations = SupportingOrganisation.all.has_logo.order(:position)
+    @signatories = Signatory.all.order(:position)
     @letter_translations = LetterTranslation.all.has_pdf
   end
 
