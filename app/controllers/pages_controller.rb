1edd3139322087516e42a2982e6d5e60d0b7dd78
Tom ten Thij <code@tomtenthij.nl>
2024-04-10 11:45:48 +0200

    Add research content block

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,7 @@ class PagesController < ApplicationController
     @page_introduction_content = content_for_key("page_introduction")
     @open_letter_content = content_for_key("open_letter")
     @the_ask_content = content_for_key("the_ask")
+    @research_content = content_for_key("research")
     @supporting_organisations = SupportingOrganisation.all.has_logo.order(:position)
     @signatories = Signatory.all.order(:position)
     @letter_translations = LetterTranslation.all.has_pdf
