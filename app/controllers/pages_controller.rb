d91c6e8711adbbc54bc5ff9f00ba634ebcc39bed
Tom ten Thij <code@tomtenthij.nl>
2024-04-11 16:02:29 +0200

    Order translations by language_name

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
     @research_content = content_for_key("research")
     @supporting_organisations = SupportingOrganisation.all.has_logo.order(:position)
     @signatories = Signatory.all.order(:position)
-    @letter_translations = LetterTranslation.all.has_pdf
+    @letter_translations = LetterTranslation.all.order(:language_name).has_pdf
   end
 
   def supporting_organisations
